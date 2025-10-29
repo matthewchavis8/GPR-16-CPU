e259de3ad821dda34f02eb366ef7a55e783d3047
matthewchavis8 <matthewchavis8@gmail.com>
2025-10-29 09:36:22 -0500

    Set up VM-translator extension  - Okay so set up overall structure for the extensin  - Added a new module Vm-Translator  this will handle    the main loop for the codewriter and parser  - Hooked it in to the root Cmake

---
+++ VM-Translator/Modules/VMTranslator/vmtranslator.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#include "../VMTranslator/vmtranslator.h"
+
+void VMTranslator::foo() {
+  std::cout << "Hello this is a foo class!\n";
+}
---
+++ VM-Translator/Modules/VMTranslator/vmtranslator.h
@@ -0,0 +1,8 @@
+
+#pragma once
+
+class VMTranslator {
+
+  void foo();
+
+};
