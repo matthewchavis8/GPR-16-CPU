46a695b0a3abca54f00aceb4adf585669cc5080b
matthewchavis8 <matthewchavis8@gmail.com>
2025-11-18 10:57:03 -0600

    Symbol Table API interface
     - Defined the API interface for the Symbol Table
     - Created a enum class to represent identifiers
     - Decided to split the table into two smaller tables
       apparanetly better caching or something need to check
      if that is real though

---
+++ Compiler/Modules/SymbolTable/symbolTable.h
@@ -0,0 +1,42 @@
+#pragma once
+
+#include <cstdint>
+#include <string>
+#include <string_view>
+#include <unordered_map>
+#include "../Utils/identifier.h"
+
+struct Symbol {
+  std::string    type;
+  IdentifierKind kind;
+  uint32_t       idx;
+};
+
+using Table = std::unordered_map<std::string, Symbol>;
+
+class SymbolTable {
+  private:
+    Table m_classScopeTable;
+    Table m_subroutineScopeTable;
+
+    uint32_t m_staticCnt{};
+    uint32_t m_fieldCnt{};
+    uint32_t m_argCnt{};
+    uint32_t m_varCnt{};
+
+  public:
+    SymbolTable();
+    SymbolTable& operator=(const SymbolTable&) = delete;
+
+    void reset();
+
+    void define(std::string_view name, std::string_view type, IdentifierKind kind);
+
+    uint32_t varCount(IdentifierKind kind) const;
+
+    IdentifierKind kindOf(std::string_view name) const;
+
+    std::string typeOf(std::string_view name) const;
+
+    uint32_t indexOf(std::string_view name) const;
+};
---
+++ Compiler/Modules/Utils/identifier.h
@@ -0,0 +1,10 @@
+#pragma once
+
+
+enum class IdentifierKind {
+  Static,
+  Field,
+  Arg,
+  Var,
+  None,
+};
